05e1858777539454c04bbda43b5dc00835e594f5
Ayesha Arshad <139641190+coding-with-ayesha@users.noreply.github.com>
2023-09-17 20:15:40 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,76 @@
+$(document).ready(function() {
+    $("#menu").click(function() {
+        $("#navbar").slideToggle("slow");
+    });
+});
+
+$(document).ready(function() {
+    $("#searchbar").click(function() {
+        $("#search").slideToggle("slow");
+    });
+});
+
+var swiper = new Swiper(".heroslider", {
+    grabCursor: true,
+    effect: "creative",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    creativeEffect: {
+        prev: {
+            shadow: false,
+            translate: [-1000, 0, 0],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
+});
+
+var swiper = new Swiper(".proslider", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+
+        790: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+
+        1080: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+
+        1280: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1550: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+        },
+        1800: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+        },
+    },
+});
+
+var swiper = new Swiper(".tesslider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
